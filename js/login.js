8577fb7b10c708f20e9ad9d4223d4892bb2dd67b
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-09-09 23:45:45 -0500

    changed code formatting to clean up nesting

--- js/login.js
+++ js/login.js
@@ -11,29 +11,31 @@ function logout() {
 }
 
 function displayControls(data) {
-    if(data.auth) {
-        const varifyAuthDiv = document.getElementById("verify_auth_result");
-        const loginForm = document.getElementById("LoginForm");
+    if (!data.auth) {
+        return;
+    }
+    
+    const varifyAuthDiv = document.getElementById("verify_auth_result");
+    const loginForm = document.getElementById("LoginForm");
 
-        loginForm.style.display = 'none';
-        varifyAuthDiv.style.display = 'block';
+    loginForm.style.display = 'none';
+    varifyAuthDiv.style.display = 'block';
 
-        const welcome = document.getElementById("Welcome");
-        welcome.innerHTML = `Welcome back ${data.username}`;
+    const welcome = document.getElementById("Welcome");
+    welcome.innerHTML = `Welcome back ${data.username}`;
 
-        const addProductLink = document.getElementById("AddProductLink");
-        const updateButtons = document.getElementsByClassName("Update");
-        const deleteButtons = document.getElementsByClassName("Delete");
+    const addProductLink = document.getElementById("AddProductLink");
+    const updateButtons = document.getElementsByClassName("Update");
+    const deleteButtons = document.getElementsByClassName("Delete");
 
-        if (addProductLink != null) {
-            addProductLink.style.display = 'block';
-        }
+    if (addProductLink != null) {
+        addProductLink.style.display = 'block';
+    }
 
-        if (updateButtons.length > 0) {
-            for (index = 0; index < updateButtons.length; index++) {
-                updateButtons[index].style.display = 'block';
-                deleteButtons[index].style.display = 'block';
-            }
+    if (updateButtons.length > 0) {
+        for (index = 0; index < updateButtons.length; index++) {
+            updateButtons[index].style.display = 'block';
+            deleteButtons[index].style.display = 'block';
         }
     }
 }
