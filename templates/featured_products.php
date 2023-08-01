cd0145b5d3f5b06c12766ca17c426e72f2657441
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-08-01 18:29:28 -0500

    modified product price to properly format value to dollar amount

--- templates/featured_products.php
+++ templates/featured_products.php
@@ -8,7 +8,7 @@ require_once "src/home_files/get_featured.php";
         <div class="productCard">
             <img src="images/<?= htmlspecialchars($product['ProductImage']) ?>">
             <h3 class="ProductName"><?= htmlspecialchars($product['ProductName']) ?></h3>
-            <p class="ProductPrice"><?= htmlspecialchars($product['ProductPrice']) ?></p>
+            <p class="ProductPrice">$<?= number_format(htmlspecialchars($product['ProductPrice']), 2) ?></p>
         </div>
     <?php endforeach ?>
 </div>
